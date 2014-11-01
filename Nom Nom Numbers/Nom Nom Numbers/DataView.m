c7a1330e4a2d814aedb0695c630d610f25be2b1f
ddemas <mediasand@dhcp-58-12.st.hmc.edu>
2014-10-31 23:54:05 -0700

    changed the initial time back to normal

--- Nom Nom Numbers/Nom Nom Numbers/DataView.m
+++ Nom Nom Numbers/Nom Nom Numbers/DataView.m
@@ -15,7 +15,7 @@
     CGFloat Xdimensions = mainScene.size.width;
     CGFloat Ydimensions = mainScene.size.height;
     
-    _initialTime = 1;
+    _initialTime = 60;
     CGFloat headerY = Ydimensions * .93;
     NSString* fontType = @"MarkerFelt-Thin";
     
