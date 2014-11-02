b1603ae89f29f905ed0e1c02b3412076676ea6d5
Shannon <shannon@WL-24-78.CINE.Claremont.Edu>
2014-11-02 12:19:48 -0800

    Changed popup text positions

--- Nom Nom Numbers/Nom Nom Numbers/GameOverButton.m
+++ Nom Nom Numbers/Nom Nom Numbers/GameOverButton.m
@@ -15,10 +15,18 @@
     CGFloat sceneX = mainScene.size.width;
     CGFloat sceneY = mainScene.size.height;
     
+    // Create popup backlay
+    UIColor* transparentColor = [[UIColor alloc] initWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
+    SKSpriteNode* gameOverBacklay = [[SKSpriteNode alloc] initWithColor:transparentColor size:CGSizeMake(sceneX, sceneY)];
+    gameOverBacklay.position = CGPointMake(sceneX * 0.5, sceneY * 0.5);
+    gameOverBacklay.zPosition = 2;
+    [self addChild:gameOverBacklay];
+    
     // Create popup
     SKSpriteNode* gameOverPopup = [[SKSpriteNode alloc] initWithImageNamed:@"wooden"];
     gameOverPopup.size = CGSizeMake(sceneX * 0.35, sceneY * 0.3);
     gameOverPopup.position = CGPointMake(sceneX * 0.5, sceneY * 0.5);
+    gameOverPopup.zPosition = 2;
     [self addChild:gameOverPopup];
     
     
@@ -30,24 +38,33 @@
     gameOverPopupTitle.fontColor = [UIColor whiteColor];
     gameOverPopupTitle.fontSize = 35;
     gameOverPopupTitle.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
-    gameOverPopupTitle.position = CGPointMake(0, popupY * 0.3);
+    gameOverPopupTitle.position = CGPointMake(0, popupY * 0.35);
     gameOverPopupTitle.text = @"Game Over!";
     [gameOverPopup addChild:gameOverPopupTitle];
     
     // Create score content text on the popup
     SKLabelNode* gameOverPopupScore = [[SKLabelNode alloc] initWithFontNamed:@"MarkerFelt-Thin"];
     gameOverPopupScore.fontColor = [UIColor whiteColor];
-    gameOverPopupScore.fontSize = 30;
-    gameOverPopupScore.position = CGPointMake(0, popupY * 0.1);
+    gameOverPopupScore.fontSize = 28;
+    gameOverPopupScore.position = CGPointMake(0, popupY * 0.17);
     gameOverPopupScore.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
-    gameOverPopupScore.text = [NSString stringWithFormat:@"Your score was %.3f.", currentScore];
+    gameOverPopupScore.text = [NSString stringWithFormat:@"Your score was"];
     [gameOverPopup addChild:gameOverPopupScore];
+    
+    // Create score text on the popup
+    SKLabelNode* gameOverScore = [[SKLabelNode alloc] initWithFontNamed:@"MarkerFelt-Thin"];
+    gameOverScore.fontColor = [UIColor whiteColor];
+    gameOverScore.fontSize = 33;
+    gameOverScore.position = CGPointMake(0, popupY * 0.2);
+    gameOverScore.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
+    gameOverScore.text = [NSString stringWithFormat:@"%.3f", currentScore];
+    [gameOverPopup addChild:gameOverScore];
     
     // Create question content text on the popup
     SKLabelNode* gameOverPopupQuestion = [[SKLabelNode alloc] initWithFontNamed:@"MarkerFelt-Thin"];
     gameOverPopupQuestion.fontColor = [UIColor whiteColor];
-    gameOverPopupQuestion.fontSize = 30;
-    gameOverPopupQuestion.position = CGPointMake(0, popupY * -0.06);
+    gameOverPopupQuestion.fontSize = 28;
+    gameOverPopupQuestion.position = CGPointMake(0, popupY * -0.13);
     gameOverPopupQuestion.verticalAlignmentMode = SKLabelVerticalAlignmentModeCenter;
     gameOverPopupQuestion.text = [NSString stringWithFormat:@"Play again?"];
     [gameOverPopup addChild:gameOverPopupQuestion];
@@ -55,7 +72,7 @@
     // Create restart button (play again) on popup
     SKSpriteNode* restartButton = [[SKSpriteNode alloc] initWithImageNamed:@"wooden"];
     restartButton.size = CGSizeMake(popupX * 0.3, popupY * 0.2);
-    restartButton.position = CGPointMake(popupX * -0.3, popupY * -0.3);
+    restartButton.position = CGPointMake(popupX * -0.3, popupY * -0.35);
     [gameOverPopup addChild:restartButton];
     
     // Create the label on the confirmation button
@@ -70,7 +87,7 @@
     // Create quit button (return to main screen) on popup
     SKSpriteNode* quitButton = [[SKSpriteNode alloc] initWithImageNamed:@"wooden"];
     quitButton.size = CGSizeMake(popupX * 0.3, popupY * 0.2);
-    quitButton.position = CGPointMake(popupX * 0.3, popupY * -0.3);
+    quitButton.position = CGPointMake(popupX * 0.3, popupY * -0.35);
     [gameOverPopup addChild:quitButton];
     
     // Create the label on the confirmation button
